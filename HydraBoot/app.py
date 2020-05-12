2f86884b07a9ad5c1a89d374798a27568524ae16
aflorencio <aflorencio@aflorencio>
2020-05-12 18:40:20 +0200

    Change host, read error and Docker pre

--- HydraBoot/app.py
+++ HydraBoot/app.py
@@ -16,21 +16,21 @@ wsgi_app = app.wsgi_app
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return "Hello HydraBoot!"
+    return "Hola HydraBoot!"
 
 @app.route('/<serviceName>')
 def services(serviceName):
     my_path = os.path.abspath(os.path.dirname(__file__))
     path = os.path.join(my_path, "./config/boot.yml")
-    with open(path) as file:
+    with open(path, encoding="utf8", errors='ignore') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
     return config["services"][serviceName]
 
 if __name__ == '__main__':
    
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '1000'))
     except ValueError:
-        PORT = 5555
+        PORT = 1000
     app.run(HOST, PORT)
