e760092696f6589a1709ede981c98289427f3906
aflorencio <aflorencio@aflorencio>
2020-05-28 17:10:19 +0200

    Added HydraBoot server in C# language with google proto libraries.

---
+++ HydraBoot.Server/Core/MainCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HydraBoot.Server
+{
+    public class MainCore
+    {
+        public MainCore()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ HydraBoot.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HydraBoot.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("> Starting HydraBoot");
+        }
+    }
+}
